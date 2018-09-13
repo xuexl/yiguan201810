8aa07077cb82ecd31afecb852d4d5b11b9d54bf7
medo_xxl <medo_xxl@126.com>
2018-09-13 21:57:07 +0800

    分析

--- rs.R
+++ rs.R
@@ -1,3 +1,9 @@
  
-setwd('F:\\study\\tinymind\\data')
+#setwd('F:\\study\\tinymind\\data')
+setwd('G:\\tinymind\\data')
+
+brandData<-read.table('deviceid_brand.tsv', sep=' ',encoding='utf8',header=F)
+
 dt<-read.table('deviceid_train.csv', sep='\t',encoding='utf8',header=F)
+
+
