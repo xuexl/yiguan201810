adb455f837ef25d6f37817637ec9ba459659dbfc
medo_xxl <medo_xxl@126.com>
2018-09-14 05:50:35 +0800

    分析

--- rs.R
+++ rs.R
@@ -6,4 +6,5 @@ brandData<-read.table('deviceid_brand.tsv', sep=' ',encoding='utf8',header=F)
 
 dt<-read.table('deviceid_train.csv', sep='\t',encoding='utf8',header=F)
 
-
+appStartClose<-file('deviceid_package_start_close.tsv', 'r')
+readLines(appStartClose, n=1)
