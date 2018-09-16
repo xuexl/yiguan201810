7647acd0c31c2c0d03fd7b5f9943dc9f574b4264
medo_xxl <medo_xxl@126.com>
2018-09-17 05:46:43 +0800

    分析

--- rs.R
+++ rs.R
@@ -2,9 +2,53 @@
 #setwd('F:\\study\\tinymind\\data')
 setwd('G:\\tinymind\\data')
 
-brandData<-read.table('deviceid_brand.tsv', sep=' ',encoding='utf8',header=F)
+#brand
+#对品牌文件处理，如果有四列，中间两列要合并
+arrangeDeviceBrand <- function (){
+    newFileBrand <- file('deviceid_brand_new.tsv', 'w')
+    
+    fileBrand <- file('deviceid_brand.tsv', 'r')
+    while(TRUE){
+        rl <- readLines(fileBrand, n=1)   
+        if(length(rl) == 0)
+            break
+        else {
+            #以空格分割
+            cl <- strsplit(rl, ' ')[[1]]
+            if(length(cl) > 3){
+                r <- paste(ee[1], paste(ee[c(2:(length(cl)-1))], collapse = '_'), ee[length(cl)], sep=' ')
+                # print(paste(ee[c(2:length(cl)-1)], sep=' ', collapse = ' '))
+            }
+            else {
+                r <- rl
+                # print(rl)
+            }
+            #写入文件
+            writeLines(r, newFileBrand)
+        }
+    }
+    close(newFileBrand)
+}
 
-dt<-read.table('deviceid_train.csv', sep='\t',encoding='utf8',header=F)
+arrangeDeviceBrand()
 
-appStartClose<-file('deviceid_package_start_close.tsv', 'r')
-readLines(appStartClose, n=1)
+
+
+
+
+
+dd <- "8d470dc6253176a1194b31a0a3c23c65 Huawei HUAWEI G660-L075"  
+
+ee <- strsplit(dd, ' ')[[1]]
+ee[c(2:3)]
+paste(c(2:3), sep='_')
+paste(ee[c(2:(length(ee) - 1))], collapse = '_')
+
+
+?paste
+print(1)
+
+
+# brandData<-read.table('deviceid_brand.tsv', sep=' ',encoding='utf8',header=F)
+
+
