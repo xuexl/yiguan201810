f45810ee0f0baa0029e16025dd09ccaa3f3a22df
medo_xxl <medo_xxl@126.com>
2018-09-19 05:45:19 +0800

    分析

--- rs.R
+++ rs.R
@@ -5,29 +5,30 @@ setwd('G:\\tinymind\\data')
 #brand
 #对品牌文件处理，如果有四列，中间两列要合并
 arrangeDeviceBrand <- function (){
-    newFileBrand <- file('deviceid_brand_new.tsv', 'w')
+    newFileBrand <- file('deviceid_brand_new.tsv', 'w', encoding = 'UTF-8')
     
-    fileBrand <- file('deviceid_brand.tsv', 'r')
-    while(TRUE){
+    fileBrand <- file('deviceid_brand.tsv', 'r', encoding = 'UTF-8')
+    seek(con=fileBrand, where=0)
+    repeat {
         rl <- readLines(fileBrand, n=1)   
-        if(length(rl) == 0)
-            break
-        else {
-            #以空格分割
-            cl <- strsplit(rl, ' ')[[1]]
-            if(length(cl) > 3){
-                r <- paste(ee[1], paste(ee[c(2:(length(cl)-1))], collapse = '_'), ee[length(cl)], sep=' ')
-                # print(paste(ee[c(2:length(cl)-1)], sep=' ', collapse = ' '))
-            }
-            else {
-                r <- rl
-                # print(rl)
-            }
-            #写入文件
-            writeLines(r, newFileBrand)
-        }
+        if(length(rl) == 0) break
+        
+        #以空格分割
+        # cl <- strsplit(rl, ' ')[[1]]
+        # if(length(cl) > 3){
+        #     r <- paste(ee[1], paste(ee[c(2:(length(cl)-1))], collapse = '_'), ee[length(cl)], sep=' ')
+        #     # print(paste(ee[c(2:length(cl)-1)], sep=' ', collapse = ' '))
+        # }
+        # else {
+        #     r <- rl
+        #     # print(rl)
+        # }
+        #写入文件
+        r <- rl
+        writeLines(r, newFileBrand, sep='\n')
     }
     close(newFileBrand)
+    close(fileBrand)
 }
 
 arrangeDeviceBrand()
@@ -48,6 +49,8 @@ paste(ee[c(2:(length(ee) - 1))], collapse = '_')
 ?paste
 print(1)
 
+?file
+?readLines
 
 # brandData<-read.table('deviceid_brand.tsv', sep=' ',encoding='utf8',header=F)
 
